3439bcf5116be4c0e67d34ff9f9b1c855840fab9
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-07-19 17:18:33 +0100

    got a card swipper into home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:grocery_app/provider/dark_theme_provider.dart';
+import 'package:grocery_app/services/utils.dart';
 import 'package:provider/provider.dart';
+import 'package:card_swiper/card_swiper.dart';
+
+import '../provider/dark_theme_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,22 +13,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<String> _offerImages = [
+    "assets/images/offres/Offer1.jpg",
+    "assets/images/offres/Offer2.jpg",
+    "assets/images/offres/Offer3.jpg",
+    "assets/images/offres/Offer4.jpg",
+  ];
   @override
   Widget build(BuildContext context) {
     final themeState = Provider.of<DarkThemeProvider>(context);
+    final Size screenSize = Utils(context).getScreenSize;
+    final height = screenSize.height;
     return Scaffold(
-      body: Center(
-        child: SwitchListTile(
-          title: Text(themeState.getDarkTheme ? "Dark Theme" : "Light Theme"),
-          secondary: Icon(themeState.getDarkTheme
-              ? Icons.dark_mode_outlined
-              : Icons.light_mode_outlined),
-          onChanged: (bool value) {
-            setState(() {
-              themeState.setDarkTheme = value;
-            });
+      body: SizedBox(
+        height: height * .33,
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return Image.asset(
+              _offerImages[index],
+              fit: BoxFit.fill,
+            );
           },
-          value: themeState.getDarkTheme,
+          itemCount: _offerImages.length,
+          pagination: const SwiperPagination(
+            alignment: Alignment.bottomCenter,
+            builder: DotSwiperPaginationBuilder(
+              color: Colors.white,
+              activeColor: Colors.red,
+            ),
+          ),
+          control: const SwiperControl(color: Colors.amber),
+          autoplay: true,
         ),
       ),
     );
